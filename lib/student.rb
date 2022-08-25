f144475ab397eecc9af4df96c488a04c123b6e6b
Allan <kipropsallan@gmail.com>
2022-08-25 16:37:56 +0300

    commit tree d4f435a755d82e3d19e5193663e52cb6437d33ac

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user'
+
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(soma)
+        @knowledge << soma
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
